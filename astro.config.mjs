4e2da33ee7a359267fabb89afa93bec671836c51
Moritz Stückler <moritz@bitbetter.de>
2024-02-20 23:56:00 +0100

    fix: github pages config

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,5 +6,7 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   // Enable React to support React JSX components.
+  site: 'https://preya.github.io/',
+  base: '/cursed-programming-stickers-site',
   integrations: [react(), tailwind()]
 });
