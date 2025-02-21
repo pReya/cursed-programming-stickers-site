d4551e6e270b652be85bd6450b0e914a66c66a34
Moritz Stückler <moritz@bitbetter.de>
2025-02-21 10:43:06 +0100

    fix: base url and folder

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  // Enable React to support React JSX components.
+  site: 'https://preya.github.io/',
+  base: '/cursed-programming-stickers-site',
   integrations: [react(), tailwind()]
 });
